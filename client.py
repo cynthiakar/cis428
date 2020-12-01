5ac4fe58f4755bf461e2c38cc6f2700f91c53628
Andrew <andrewhamann1999@gmail.com>
2020-12-01 13:26:37 -0500

    client ip change test

--- client.py
+++ client.py
@@ -3,9 +3,10 @@ import socket
 #RASPBERRY PI
 
 CLIENT_IP = '192.168.0.19'
+SERVER_IP = '192.168.0.5'
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # client.connect(('0.0.0.0', 8080)) #localhost
-client.connect((CLIENT_IP, 8080)) #localhost
+client.connect((SERVER_IP, 8080)) 
 client.send(b'I am CLIENT<br>')
 from_server = client.recv(4096)
 client.close()
