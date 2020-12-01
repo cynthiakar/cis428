511823b41ad4be4992762a394de979733f36abd0
Andrew <andrewhamann1999@gmail.com>
2020-12-01 15:54:41 -0500

    json load streaming

--- client.py
+++ client.py
@@ -19,19 +19,19 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # client.connect(('0.0.0.0', 8080)) #localhost
 client.connect((SERVER_IP, 8080))
 client.send(b'I am CLIENT<br>')
-pickled_size = client.recv(8192)
-size = json.loads(pickled_size)
+ser_size = client.recv(28)
+size = json.loads(ser_size.decode())
 print(size)
 client.send(b'Recieved Size<br>')
 
-# pickled_rawPitchList = b''
-# l = size
-# while l > 0:
-#     d = client.recv(l)
-#     l -= len(d)
-#     pickled_rawPitchList += d
-# pickled_rawPitchList = client.recv(size)
-# rawPitchList = pickle.loads(pickled_rawPitchList)
-# client.send(b'Recieved pitchList<br>')
+ser_rawPitchList = b''
+l = size
+while l > 0:
+    d = client.recv(l)
+    l -= len(d)
+    ser_rawPitchList += d
+# ser_rawPitchList = client.recv(size)
+rawPitchList = json.loads(ser_rawPitchList.decode())
+client.send(b'Recieved pitchList<br>')
 client.close()
 print(rawPitchList)
