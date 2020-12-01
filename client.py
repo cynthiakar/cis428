95bec4824d5d8535a9a7ab10a2f314018a5fc76b
Andrew <andrewhamann1999@gmail.com>
2020-12-01 15:36:08 -0500

    decode removed

--- client.py
+++ client.py
@@ -20,7 +20,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER_IP, 8080))
 client.send(b'I am CLIENT<br>')
 pickled_size = client.recv(8192)
-size = json.loads(pickled_size.decode())
+size = json.loads(pickled_size)
 print(size)
 client.send(b'Recieved Size<br>')
 
