c6a2c28c358c2779c3716fcd0a9f17aab02ec5c2
cynthiakar <duckie888@gmail.com>
2020-12-01 22:06:13 -0800

    added handshake and digital signatures

--- client.py
+++ client.py
@@ -1,48 +1,115 @@
-import socket
-import json
+import os, socket, json
 from multiprocessing.connection import Listener
 from ast import literal_eval
 from soundUtil import SoundUtil
 from soundAnalysis import SoundAnalysis
 from record import recordWAV
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
+
 #RASPBERRY PI
 
-# connect to socket and listen for rawWAV pitchList
-CLIENT_IP = '192.168.0.19'
-SERVER_IP = '192.168.0.5'
+# CONNECT TO SOCKET
+CLIENT_IP = '192.168.0.19' # raspberry pi
+SERVER_IP = '192.168.0.5' # mac laptop
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect(('0.0.0.0', 8080)) #localhost
-client.connect((SERVER_IP, 8080))
-client.send(b'I am CLIENT<br>')
-# ser_size = client.recv(4)
-# size = json.loads(ser_size.decode())
-# print(size)
-# client.send(b'Recieved Size<br>')
+client.connect(('0.0.0.0', 8080)) #localhost
+# client.connect((SERVER_IP, 8080))
 
-# ser_rawPitchList = b''
-# l = size-47
-# while l > 0:
-#     d = client.recv(l)
-#     l -= len(d)
-#     ser_rawPitchList += d
-ser_soundSequence = client.recv(88)
-soundSequence = json.loads(ser_soundSequence.decode())
-client.send(b'Recieved soundSequence<br>')
-client.close()
+# START HANDSHAKE PROTOCOL
+# public key and private key generation
+random_generator = Random.new().read
+key = RSA.generate(1024,random_generator)
+public = key.publickey().exportKey()
+private = key.exportKey()
+
+# hash public key
+hash_object = SHA256.new(public)
+hex_digest = hash_object.hexdigest()
+
+# send unhashed public key
+client.send(public)
+
+# receive confirmation
+conf = client.recv(3)
+print(conf)
+session_key = None
+if conf == b'YES':
+	print("public key received")
+	# send hashed public key
+	client.send(hex_digest.encode())
+
+	# receive encrypted session key and public key 
+	msg = client.recv(1024).decode()
+	en = eval(msg)
+
+	# decrypt using RSA key
+	decrypt = key.decrypt(en)
+	# hashing session key
+	en_object = SHA256.new(decrypt)
+	en_digest = en_object.hexdigest()
+
+	session_key = en_digest
+
+	print("\n-----ENCRYPTED PUBLIC KEY AND SESSION KEY FROM SERVER-----")
+	print(msg)
+	print("\n-----DECRYPTED SESSION KEY-----")
+	print(session_key)
+	print("\n-----HANDSHAKE COMPLETE-----\n")
+# FINISH HANDSHAKE PROTOCOL
+
+# RECEIVE ENCRYPTED SOUND SEQUENCE
+en_soundSequence = client.recv(1024)
+# decrypt soundSequence using session key
+key = session_key[:16]
+iv = en_soundSequence[:16]
+ctr = Counter.new(128, initial_value=int.from_bytes(iv, byteorder='big'))
+cipher = AES.new(key,AES.MODE_CTR,counter = ctr)
+decrypted_message = cipher.decrypt(en_soundSequence[16:])
+# deserialize soundsequence
+soundSequence = json.loads(decrypted_message.decode())
+client.send(b'Recieved soundSequence')
+# load as array of floats
 soundSequence = literal_eval(soundSequence)
 print(soundSequence)
 
-# record sound
+# RECORD SOUND
 totalDuration = sum([d for _,d in soundSequence])
 soundUtil = SoundUtil()
 soundUtil.record("recording.wav", totalDuration)
 # decrypt package
 
-# get pitchList
+# GET PITCH LIST
 soundAnalysis = SoundAnalysis()
 recordedPitchList = soundAnalysis.getPitchList("recording.wav")
 
-# testAnalyze for pitch list comparison
+# ANALYZE SOUNDS USING PITCH LIST COMPARISON
 soundAnalysis.testAnalyze(soundSequence, recordedPitchList)
-# send encrypted verification back to server
+
+# SEND ENCRYPTED AUTHENTICATION RESPONSE
+# response is length 14
+response = "Access Granted"
+# response = "Access Denied!"
+
+# create digital signature
+h = SHA256.new(response.encode())
+# sign using private key
+signer = PKCS1_v1_5.new(RSA.importKey(private))
+signature = signer.sign(h)
+message = response.encode() + signature
+# encrypt message
+key = session_key[:16]
+iv = os.urandom(16)
+ctr = Counter.new(128, initial_value=int.from_bytes(iv, byteorder='big'))
+cipher = AES.new(key,AES.MODE_CTR,counter = ctr)
+encrypted_message = iv + cipher.encrypt(message)
+# send encrypted message
+client.send(encrypted_message)
+print("Response sent:", response)
+
+client.close()
--- server.py
+++ server.py
@@ -1,11 +1,15 @@
-import socket
 from soundUtil import SoundUtil
 from soundAnalysis import SoundAnalysis
 import getpass
 from loginsystem import LoginSystem
 from security import encrypt_password
-import sys
-import json
+import os, sys, json, socket
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
 
 # MAC LAPTOP
 
@@ -16,54 +20,99 @@ soundSequence = soundUtil.generateRandomSound()
 soundUtil.createWAV(soundSequence)
 
 soundAnalysis = SoundAnalysis()
-# send encrypted package to Pi including rawWAVFile, digital signature?
-    # create pitch list
-pitchList = soundAnalysis.getPitchList(soundUtil.soundFile)
-print(sys.getsizeof(pitchList))
-print(sys.getsizeof(str(pitchList)))
-print(len(pitchList))
-    # encrypt
-
-    # socket and send
-CLIENT_IP = '192.168.0.19'
-SERVER_IP = '192.168.0.5'
+# CONNECT TO SOCKET
+CLIENT_IP = '192.168.0.19' # raspberry pi
+SERVER_IP = '192.168.0.5' # mac laptop
 PORT = 8080
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# serv.bind(('0.0.0.0', 8080)) # localhost
-serv.bind((SERVER_IP, 8080))
+serv.bind(('0.0.0.0', 8080)) # localhost
+# serv.bind((SERVER_IP, 8080))
 serv.listen(5)
 while True:
     conn, addr = serv.accept()
-    # receive "I am CLIENT"
+
+    # START HANDSHAKE PROTOCOL
+    # get publickey from client
+    getpbk = conn.recv(2048)
+
+    # conversion of string to key
+    server_public_key = RSA.importKey(getpbk)
+
+    # hash received public key string
+    hash_object = SHA256.new(getpbk)
+    hex_digest = hash_object.hexdigest()
+
+    # send confirmation to client
+    if getpbk != "":
+        print(getpbk)
+        conn.send(b'YES')
+        # receive hashed public key
+        gethash = conn.recv(1024)
+        print ("\n-----HASH OF PUBLIC KEY----- \n"+gethash.decode())
+
+    session_key = None
+    # check if hashes match
+    if hex_digest == gethash.decode():
+        # create session key
+        key_128 = os.urandom(16)
+        # encrypt session key using AES in CTR mode
+        encryptor = AES.new(key_128,AES.MODE_CTR,counter = lambda:key_128)
+        en_session_key = encryptor.encrypt(key_128)
+
+        # encrypt session key and public key
+        E = server_public_key.encrypt(en_session_key,16)
+        print("\n-----ENCRYPTED PUBLIC KEY AND SESSION KEY-----\n"+str(E))
+        # send encrypted session key and public key
+        conn.send(str(E).encode())
+        print("\n-----HANDSHAKE COMPLETE-----")
+
+        # hashing session key
+        en_object = SHA256.new(en_session_key)
+        session_key = en_object.hexdigest()
+
+        print("\n-----SESSION KEY-----\n"+session_key)
+    # FINISH HANDSHAKE PROTOCOL
+
+    # SEND EXPECTED SOUND SEQUENCE
+    # serialize soundSequence
+    message = json.dumps(str(soundSequence))
+    # encrypt soundSequence
+    key = session_key[:16]
+    iv = os.urandom(16)
+    ctr = Counter.new(128, initial_value=int.from_bytes(iv, byteorder='big'))
+    cipher = AES.new(key,AES.MODE_CTR,counter = ctr)
+    encrypted_message = iv + cipher.encrypt(message)
+    # send encrypted soundSequence
+    conn.send(encrypted_message)
+
+    # RECEIVE CONFIRMATION
     data = conn.recv(4096)
     print(data)
 
-    # # send array length
-    # conn.send(json.dumps(sys.getsizeof(str(soundSequence))).encode())
-    #
-    # # receive confirmation
-    # data = conn.recv(4096)
-    # print(data)
+    # PLAY SOUND
+    # if data == b'Recieved soundSequence':
+    #     soundUtil.play()
 
-    # send pitchList
-    conn.send(json.dumps(str(soundSequence)).encode())
-
-    # receive confirmation
+    # RECEIVE ENCRYPTED AUTHENTICATION RESPONSE
     data = conn.recv(4096)
-    print(data)
 
-    if data == b'Recieved soundSequence<br>':
-        break
+    # decrypt message
+    key = session_key[:16]
+    iv = data[:16]
+    ctr = Counter.new(128, initial_value=int.from_bytes(iv, byteorder='big'))
+    cipher = AES.new(key,AES.MODE_CTR,counter = ctr)
+    decrypted_message = cipher.decrypt(data[16:])
+    # verify signature with public key
+    message = decrypted_message[:14]
+    signature = decrypted_message[14:]
+    h = SHA256.new(message)
+    verifier = PKCS1_v1_5.new(server_public_key)
+    if verifier.verify(h, signature):
+        print("authentic")
+        print(message.decode())
+    else:
+        print("not authentic")
 
     # close connection
     conn.close()
     print('client disconnected')
-
-# play sound
-soundUtil.play()
-
-# waits/listens for verification
-
-# if verified grant access
-
-# else, generate sound again OR quit
